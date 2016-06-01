a5c1e9e0af2538f75b23b8806b7ff1da4d56cca9
Ray Wu <rayning.wu@gmail.com>
2016-06-01 14:26:39 -0400

    Keep gulp watching after error

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var browserify = require("browserify");
 var gulp = require("gulp");
+var gutil = require("gulp-util");
 var source = require("vinyl-source-stream");
 var babelify = require("babelify");
 var streamify = require("gulp-streamify");
@@ -11,31 +12,41 @@ var bundler = browserify({
 }).transform(babelify, {presets: ["react"]});
 
 gulp.task("prod-build", function() {
-  console.log("Building for production");
+  gutil.log("Building for production");
   return bundleWith(bundler, { uglify: true });
 });
 
 gulp.task("dev-build", function() {
-  console.log("Building for development");
+  gutil.log("Building for development");
   return bundleWith(bundler, { uglify: false });
 });
 
 // Watch for changes in source then run build task
 gulp.task("watch", function() {
-  console.log("Watching for changes");
+  gutil.log("Watching for changes");
   gulp.watch(["./scripts/**/*.js"], ["dev-build"]);
 });
 
 gulp.task("default", ["dev-build", "watch"]);
 
+// Logs error and emits end event to tell watch to keep going
+// https://github.com/gulpjs/gulp/issues/259
+function handleError(err) {
+  gutil.log(err);
+  this.emit("end");
+}
+
 // Bundles files with the provided bundler
 function bundleWith(bundler, opts) {
   opts = opts || {};
-  console.log("Bundling with options:", opts);
+  gutil.log("Bundling with options:", opts);
   var b = bundler.bundle()
-    .pipe(source("main.js"));
+    .on("error", handleError)
+    .pipe(source("main.js"))
+    .on("error", handleError);
   if (opts.uglify) {
     b = b.pipe(streamify(uglify()));
   }
-  return b.pipe(gulp.dest("./public/scripts"));
+  return b.pipe(gulp.dest("./public/scripts"))
+    .on("error", handleError);
 }
