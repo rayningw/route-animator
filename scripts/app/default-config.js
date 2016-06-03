674ee9775061078c8ffe31202a2cc3ef64ba092c
Ray Wu <rayning.wu@gmail.com>
2016-06-03 13:43:37 -0400

    Allow unlimited waypoints

--- scripts/app/default-config.js
+++ scripts/app/default-config.js
@@ -4,12 +4,13 @@ var jhu = { name: "JHU", location: { lat: 39.329910, lng: -76.620502 } };
 var kansasCity = { name: "Kansas City", location: { lat: 39.114445, lng: -94.627061 } };
 var pittsburgh = { name: "Pittsburgh", location: { lat: 40.439247, lng: -80.009310 } };
 var sparks = { name: "Sparks", location: { lat: 39.533575, lng: -119.757114 } };
+var vedauwoo = { name: "Vedauwoo Campground", location: { lat: 41.156276, lng: -105.376485 } };
 var denver = { name: "Denver", location: { lat: 39.768034, lng: -104.903520 } };
 var spaceMuseum = { name: "Space Museum", location: { lat: 41.018012, lng: -96.319971 } };
 var stLouis = { name: "St. Louis", location: { lat: 38.624138, lng: -90.184582 } };
 var louisville = { name: "Louisville", location: { lat: 38.252620, lng: -85.757743 } };
 var charlestownPark = { name: "Charlestown Park", location: { lat: 38.429801, lng: -85.628119 } };
-var waypoints = [ rockridge, sparks, denver, spaceMuseum, kansasCity, stLouis, louisville, charlestownPark, pittsburgh, jhu ];
+var waypoints = [ rockridge, sparks, vedauwoo, denver, spaceMuseum, kansasCity, stLouis, louisville, charlestownPark, pittsburgh, jhu ];
 
 // Center of the US
 var initialLocation = {lat: 37.6, lng: -95.665};
--- scripts/map-panel/map-util.js
+++ scripts/map-panel/map-util.js
@@ -1,29 +1,90 @@
 /*global google*/
 
+var _ = require("lodash");
+
+// Maximum number of waypoints, including origin and destination supported per
+// directions service route request
+var MAX_ROUTE_WAYPOINTS = 10;
+
 function getRoute(directionsService, waypoints, callback) {
+  if (waypoints.length < 2) {
+    callback("Cannot get route for less than 2 waypoints");
+    return;
+  }
+
+  var numRequests = 0;
+  var responses = [];  // [{result, status}*]
+  var numResponsesReceived = 0;
+
+  // Waypoints queued for the next request
+  var pendingRequestWaypoints = [];
+
+  // Calls the directions service route finder with the pending waypoints
+  function doPendingRequest() {
+    var request = getRouteRequest(pendingRequestWaypoints);
+    var requestIndex = numRequests++;
+    responses.push();  // Placeholder for response
+
+    directionsService.route(request, (result, status) => {
+      // Record the response for processing later
+      responses.splice(requestIndex, 0, { result: result, status: status });
+      numResponsesReceived++;
+
+      // Process the responses if we have received them all
+      if (numResponsesReceived == numRequests) {
+        processRouteResponses(responses, callback);
+      }
+    });
+  }
+
+  // Get routes from batches of waypoints
+  waypoints.forEach(waypoint => {
+    pendingRequestWaypoints.push(waypoint);
+
+    // Do the request if we have queued up enough waypoints
+    if (pendingRequestWaypoints.length == MAX_ROUTE_WAYPOINTS) {
+      doPendingRequest();
+
+      // Prep the next request's origin as the current route's destination
+      pendingRequestWaypoints = [ _.last(pendingRequestWaypoints) ];
+    }
+  });
+
+  // Do one more request with the remaining waypoints
+  if (pendingRequestWaypoints.length > 1) {
+    doPendingRequest();
+  }
+}
+
+// Processes all directions service route responses
+function processRouteResponses(responses, callback) {
+  var errs = responses
+    .filter(response => response.status != google.maps.DirectionsStatus.OK)
+    .map(response => "Received status: " + response.status);
+
+  if (errs.length > 0) {
+    callback("Received errors: " + errs);
+    return;
+  }
+
+  var responseCoords = responses.map(response => calcCoords(response.result.routes[0]));
+  callback(null, _.flatten(responseCoords));
+}
+
+function getRouteRequest(waypoints) {
   var stopovers = waypoints.slice(1, waypoints.length-1).map(function(waypoint) {
     return {
       location: waypoint
     };
   });
 
-  var request = {
+  return {
     origin: waypoints[0],
     destination: waypoints[waypoints.length-1],
     // Waypoints in the request message are actually the stopovers in the middle
     waypoints: stopovers,
     travelMode: google.maps.TravelMode.DRIVING
   };
-
-  directionsService.route(request, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      var coords = calcCoords(result.routes[0]);
-      callback(null, coords);
-    }
-    else {
-      callback("Received status: " + status);
-    }
-  });
 }
 
 function calcCoords(route) {
